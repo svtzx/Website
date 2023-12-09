a76d5936629a118e4d29361b43f6dfdcdcc9b8a8
svtzx <143448593+svtzx@users.noreply.github.com>
2023-12-09 10:50:36 +0200

    fix

--- Site/script.js
+++ Site/script.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const revealButton = document.getElementById("revealButton");
     const audio = document.getElementById("myAudio");
+    const body = document.body;
+    const revealedContent = document.querySelector(".first-container");
 
     revealButton.addEventListener("click", function () {
-        document.body.classList.add("reveal");
+        body.classList.add("reveal");
         revealButton.classList.add("hidden");
-        document.querySelector(".first-container").classList.remove("hidden");
+        revealedContent.classList.remove("hidden");
         audio.play();
-
-        // Additional code to update content dynamically
-        updateContent();
     });
-
-    function updateContent() {
-        // Add logic to dynamically update content here
-        // For example, you can toggle the visibility of different sections or replace content
-    }
 });
