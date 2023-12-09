44828cef04d999e1d00b16d8a75dc533bae9f6d1
svtzx <143448593+svtzx@users.noreply.github.com>
2023-12-09 10:29:28 +0200

    Fixes

--- Site/script.js
+++ Site/script.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the button and audio elements
     const revealButton = document.getElementById("revealButton");
     const audio = document.getElementById("myAudio");
 
-    // Add a click event listener to the button
     revealButton.addEventListener("click", function () {
-        // Show the content by removing the class that hides it
+        document.body.classList.add("reveal");
+        revealButton.classList.add("hidden");
         document.querySelector(".first-container").classList.remove("hidden");
-
-        // Play the audio
         audio.play();
     });
 });
