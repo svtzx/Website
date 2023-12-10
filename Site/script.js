9dbde3f5dc988f0289d4871d9954bb8b2a5edf2e
svtzx <143448593+svtzx@users.noreply.github.com>
2023-12-10 12:27:08 +0200

    a

--- Site/script.js
+++ Site/script.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to load additional content and start audio playback
     function loadAdditionalContent() {
-        // Use fetch to load the content from the specified URL
         fetch('https://svtzx.com/Site/styles/maincontent.html')
             .then(response => response.text())
             .then(data => {
-                // Replace the entire document's HTML with the loaded HTML
                 document.documentElement.innerHTML = data;
 
-                // Start audio playback
                 const audio = document.getElementById('autoplayAudio');
                 if (audio) {
                     audio.play();
@@ -17,13 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error('Error fetching content:', error));
     }
 
-    // Button click event listener
     var btn = document.querySelector('.btn');
     btn.addEventListener('click', function (event) {
-        // Prevent the default behavior of the link
         event.preventDefault();
 
-        // Load additional content and start audio playback when the button is clicked
+    
         loadAdditionalContent();
     });
 });
