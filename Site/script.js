318d0cc1d2ad0f874c9c89010c3309ee6878aebe
svtzx <143448593+svtzx@users.noreply.github.com>
2023-12-11 11:46:23 +0200

    a

--- Site/script.js
+++ Site/script.js
@@ -1,18 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     function loadAdditionalContent() {
         fetch('https://svtzx.com/Site/styles/maincontent.html')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch content: ${response.status} ${response.statusText}`);
-                }
-                return response.text();
-            })
+            .then(response => response.text())
             .then(data => {
-                const tempDiv = document.createElement('div');
-                tempDiv.innerHTML = data;
-                document.body.innerHTML = tempDiv.innerHTML;
+                document.documentElement.innerHTML = data;
 
-                // AUDIO //
                 const audio = document.getElementById('autoplayAudio');
                 if (audio) {
                     audio.play();
@@ -20,11 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .catch(error => console.error('Error fetching content:', error));
     }
+
     var btn = document.querySelector('.btn');
-    if (btn) {
-        btn.addEventListener('click', function (event) {
-            event.preventDefault();
-            loadAdditionalContent();
-        });
-    }
+    btn.addEventListener('click', function (event) {
+        event.preventDefault();
+        loadAdditionalContent();
+    });
 });
